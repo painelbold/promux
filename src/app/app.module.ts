b2cc58b7adb8f22dfb8267aa05607d9f62182713
App Painel Bold <app.painelbold@gmail.com>
2018-09-23 21:43:29 -0300

    Adicionada página para login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,20 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LoginPageModule } from '../pages/account/login/login.module';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    LoginPageModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
