c71ea577f2697e5ec3d26d7202b2c90153c6b5a4
App Painel Bold <app.painelbold@gmail.com>
2018-12-12 18:58:28 -0200

    Inserção dos menus laterais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Component, ViewChild } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Nav, Platform } from 'ionic-angular';
+
+import { AboutPage } from '../pages/about/about';
 import { LoginPage } from '../pages/account/login/login';
+import { PrivacyPage } from '../pages/privacy/privacy';
+import { TermsOfServicePage } from '../pages/terms-of-service/terms-of-service';
+import { MyProfilePage } from '../pages/account/my-profile/my-profile';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = LoginPage;
+  pages: Array<{title: string, component: any, icon: string}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +25,26 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.createPages();
+  }
+
+  createPages(){
+    this.pages = [
+      { title: 'Meu Perfil', component: MyProfilePage, icon: 'person'},
+      { title: 'Termos de Uso', component: TermsOfServicePage, icon: 'book' },
+      { title: 'Política de Privacidade', component: PrivacyPage, icon: 'information-circle' },
+      { title: 'Sobre o Promux', component: AboutPage, icon: 'help' },
+    ];
+  }
+
+  openPage(page){
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  logout(){
+    this.nav.setRoot(LoginPage);
   }
 }
-
