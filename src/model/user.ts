0e8466dd18c78dc175f29ae66eef9bc8f6ca5944
App Painel Bold <app.painelbold@gmail.com>
2018-12-12 18:58:05 -0200

    Criado model para dados do usuário

---
+++ src/model/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  fullName: string;
+  email: string;
+  gender: string;
+  height: number;
+  weight: number;
+  skinColor: string;
+  eyeColor: string;
+  hairColor: string;
+  dateOfBirth: Date;
+  key: string;
+}
