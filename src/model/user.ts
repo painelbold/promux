941c5be8139c7460f22de672e2f916c1a685c7ad
App Painel Bold <app.painelbold@gmail.com>
2018-12-13 18:58:50 -0200

    Adição de construtor

--- src/model/user.ts
+++ src/model/user.ts
@@ -8,5 +8,13 @@ export class User {
   eyeColor: string;
   hairColor: string;
   dateOfBirth: Date;
+  completeProfile: boolean;
   key: string;
+
+  constructor(fullName: string, email: string, dateOfBirth: Date){
+    this.fullName = fullName;
+    this.email = email;
+    this.dateOfBirth = dateOfBirth;
+    this.completeProfile = false;
+  }
 }
