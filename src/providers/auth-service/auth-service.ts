51461bd63610e56ed2181d70cfc95c1ca64a0c1e
App Painel Bold <app.painelbold@gmail.com>
2018-12-13 19:04:43 -0200

    Criação de lógica para login e registro

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -1,17 +1,52 @@
-import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
+
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
-/*
-  Generated class for the AuthServiceProvider provider.
+// import { FirebaseService } from './firebase.service';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthServiceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthServiceProvider Provider');
+  constructor(
+    // private firebaseService: FirebaseService
+    private angularFireAuth: AngularFireAuth,
+    ){}
+
+  getLoggedUser(){
+      return this.angularFireAuth.auth.currentUser;
   }
 
+  doRegister(value){
+   return new Promise<any>((resolve, reject) => {
+     firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
+     .then(
+       res => resolve(res),
+       err => reject(err))
+   })
+  }
+
+  doLogin(value){
+   return new Promise<any>((resolve, reject) => {
+     firebase.auth().signInWithEmailAndPassword(value.email, value.password)
+     .then(
+       res => resolve(res),
+       err => reject(err))
+   })
+  }
+
+  doLogout(){
+    return new Promise((resolve, reject) => {
+      if(firebase.auth().currentUser){
+        firebase.auth().signOut()
+        .then(() => {
+          // this.firebaseService.unsubscribeOnLogOut();
+          resolve();
+        }).catch((error) => {
+          reject();
+        });
+      }
+    })
+  }
 }
