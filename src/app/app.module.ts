5e0dc4fd776206ee2584906f281bd2b247c1c007
App Painel Bold <app.painelbold@gmail.com>
2018-12-13 19:05:10 -0200

    Adição de interfaces e configurações do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,30 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { AboutPageModule } from '../pages/about/about.module';
 import { LoginPageModule } from '../pages/account/login/login.module';
+import { MyProfilePageModule } from '../pages/account/my-profile/my-profile.module';
+import { HomePageModule } from '../pages/home/home.module';
+import { PrivacyPageModule } from '../pages/privacy/privacy.module';
+import { TermsOfServicePageModule } from '../pages/terms-of-service/terms-of-service.module';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import { UserDataProvider } from '../providers/user-data/user-data';
 import { RegisterPageModule } from './../pages/account/register/register.module';
 import { MyApp } from './app.component';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUavNtrtqX_wUmwIl6UKXJrSZFbzn2Ceg",
+  authDomain: "promux-cbf60.firebaseapp.com",
+  databaseURL: "https://promux-cbf60.firebaseio.com",
+  projectId: "promux-cbf60",
+  storageBucket: "promux-cbf60.appspot.com",
+  messagingSenderId: "782758598685"
+};
 
 @NgModule({
   declarations: [
@@ -15,8 +34,16 @@ import { MyApp } from './app.component';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
     LoginPageModule,
-    RegisterPageModule
+    RegisterPageModule,
+    AboutPageModule,
+    TermsOfServicePageModule,
+    PrivacyPageModule,
+    HomePageModule,
+    MyProfilePageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -25,7 +52,9 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserDataProvider,
+    AuthServiceProvider
   ]
 })
 export class AppModule {}
