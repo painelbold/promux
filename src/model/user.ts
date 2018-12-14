6c9fd636e25bdf43c2317c939f213b0937c5f3af
App Painel Bold <app.painelbold@gmail.com>
2018-12-14 17:05:27 -0200

    Inclusão de tipo de usuário

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,6 +1,9 @@
-export class User {
+import { IUser, UserType } from './IUser';
+
+export class User implements IUser {
   fullName: string;
   email: string;
+  key: string;
   gender: string;
   height: number;
   weight: number;
@@ -9,12 +12,13 @@ export class User {
   hairColor: string;
   dateOfBirth: Date;
   completeProfile: boolean;
-  key: string;
+  type: UserType
 
   constructor(fullName: string, email: string, dateOfBirth: Date){
     this.fullName = fullName;
     this.email = email;
     this.dateOfBirth = dateOfBirth;
     this.completeProfile = false;
+    this.type = UserType.PessoaFisica;
   }
 }
