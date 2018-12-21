26e7c43d56d2665cc9a51430f4ca5654661a7a65
App Painel Bold <app.painelbold@gmail.com>
2018-12-21 20:45:07 -0200

    Criação de objeto com as características das pessoas para o job

---
+++ src/model/modelAttributes.ts
@@ -0,0 +1,31 @@
+export enum PersonHairColor{
+  Indiferente,
+  Castanho,
+  Loiro,
+  Ruivo,
+  Preto,
+}
+
+export enum PersonEyeColor{
+  Indiferente,
+  Azul,
+  Castanho,
+  Mel,
+  Preto,
+  Verde
+}
+
+export enum PersonGender{
+  Indiferente,
+  Feminino,
+  Masculino
+}
+
+export enum PersonSkinColor{
+  Indiferente,
+  Asiatica,
+  Branca,
+  Indigena,
+  Negra,
+  Parda
+}
