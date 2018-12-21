052f66091b17bf46125b8bf897cfc96aed065e63
App Painel Bold <app.painelbold@gmail.com>
2018-12-21 20:35:02 -0200

    Inclusão de componentes

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from 'ionic-angular';
 import { PromuxHeaderComponent } from './promux-header/promux-header';
 import { FacebookLoginComponent } from './facebook-login/facebook-login';
+import { CompanyJobsComponent } from './company-jobs/company-jobs';
 @NgModule({
 	declarations: [GoogleLoginComponent,
     PromuxHeaderComponent,
-    FacebookLoginComponent],
+    FacebookLoginComponent,
+    CompanyJobsComponent],
 	imports: [IonicModule],
 	exports: [GoogleLoginComponent,
     PromuxHeaderComponent,
-    FacebookLoginComponent]
+    FacebookLoginComponent,
+    CompanyJobsComponent]
 })
 export class ComponentsModule {}
