c6c8cf45f1bdbbacd6cdd9c3663635fd84221af8
App Painel Bold <app.painelbold@gmail.com>
2018-12-21 20:14:07 -0200

    Adição de campos para datas. Reformulada a estrutura de criação do form

--- src/pages/job-plan/plan-step1/plan-step1.ts
+++ src/pages/job-plan/plan-step1/plan-step1.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { JobType } from "../../../model/job";
-import { ValidaCadastroProvider } from '../../../providers/valida-cadastro/valida-cadastro';
+
+import { JobType, Job } from "../../../model/job";
+import { User } from "../../../model/user";
+import { ValidaCadastroProvider } from "../../../providers/valida-cadastro/valida-cadastro";
 
 /**
  * Generated class for the PlanStep1Page page.
@@ -19,32 +21,47 @@ import { ValidaCadastroProvider } from '../../../providers/valida-cadastro/valid
 export class PlanStep1Page {
   step1Form: FormGroup;
   jobTypes: Array<{ name: string; value: JobType }>;
+  minDate: string;
+  maxCalDate: string;
+  minEndDate: string;
+
+  company: User;
+  job: Job;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private formBuilder: FormBuilder,
-    private cadastro: ValidaCadastroProvider,
+    private cadastro: ValidaCadastroProvider
   ) {
+    this.job = new Job();
     this.getJobTypes();
     this.createForm();
   }
 
-  ionViewDidLoad() {}
+  ionViewDidLoad() {
+    this.minDate = new Date().toISOString();
+    this.minEndDate = this.minDate;
+    this.maxCalDate = "2030-12-31";
+    this.company = JSON.parse(localStorage.getItem("loggedUser"));
+  }
 
   submitStep1() {
+    this.getFormValues();
     this.cadastro.setEnableStep1(false);
     this.cadastro.setEnableStep2(true);
-    localStorage.setItem("job", JSON.stringify(this.step1Form.value));
+    localStorage.setItem("job", JSON.stringify(this.job));
     this.navCtrl.parent.select(1);
   }
 
   createForm() {
     this.step1Form = this.formBuilder.group({
-      title: [""],
-      type: [""],
-      description: [""],
-      local: [""]
+      title: new FormControl(this.job.title || "", [Validators.required]),
+      type: new FormControl(this.job.type || "", [Validators.required]),
+      description: new FormControl(this.job.description || "", [Validators.required]),
+      local: new FormControl(this.job.local || "", [Validators.required]),
+      startDate: new FormControl(this.job.startDate || "", [Validators.required]),
+      endDate: new FormControl(this.job.endDate || ""),
     });
   }
 
@@ -65,5 +82,27 @@ export class PlanStep1Page {
       { name: "Panfletagem", value: JobType.Panfletagem },
       { name: "Vitrine Viva", value: JobType.VitrineViva }
     ];
+  }
+
+  getFormValues() {
+    let formValues = this.step1Form.value;
+
+    this.job.companyKey = this.company.key;
+    this.job.companyName = this.company.fullName;
+    this.job.description = formValues.description;
+    this.job.title = formValues.title;
+    this.job.type = formValues.type;
+    this.job.local = formValues.local;
+    this.job.startDate = formValues.startDate;
+    this.job.endDate = formValues.endDate;
+  }
+
+  dtInicioChange(){
+    this.minEndDate = this.step1Form.controls["startDate"].value;
+    if(this.minEndDate > this.step1Form.controls["endDate"].value){
+      this.step1Form.patchValue({
+        endDateTrip: ''
+      });
+    }
   }
 }
